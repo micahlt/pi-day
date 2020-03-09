b838c1342485d6e4212bd142003a35e2b8a5545d
Micah Lindley <micahincorporated@gmail.com>
2020-03-09 16:35:26 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,84 @@
+var body = document.getElementById("body");
+var filled = false;
+var i = 2;
+var height = 100;
+var autoOn = false;
+var heightString;
+var myInterval;
+var pi;
+var xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+       pi = JSON.parse(this.responseText);
+       pi = pi["pi"];
+    }
+};
+xhttp.open("GET", "pi.json", true);
+xhttp.send();
+document.getElementById("logo").addEventListener("click", function() {
+  nextDigit();
+});
+
+document.getElementById("body").addEventListener("keypress", function(){
+  nextDigit();
+})
+
+function nextDigit() {
+  i++;
+  let documentNode = document.createElement("H1");
+  documentNode.innerHTML = pi[i];
+  document.getElementById("pis").appendChild(documentNode);
+  window.scrollBy(0, 50);
+}
+
+function fullScreen() {
+  /*
+  if (filled = false) { */
+  if (body.requestFullscreen) {
+    body.requestFullscreen();
+    filled = true;
+    body.style.width = "100%";
+  } else if (body.mozRequestFullScreen) {
+    body.mozRequestFullScreen();
+    filled = true;
+    body.style.width = "100%";
+  } else if (body.webkitRequestFullscreen) {
+    body.webkitRequestFullscreen();
+    filled = true;
+    body.style.width = "100%";
+  } else if (body.msRequestFullscreen) {
+    body.msRequestFullscreen();
+    filled = true;
+    body.style.width = "100%";
+  } /*
+  } else {
+    if (body.exitFullscreen) {
+    body.exitFullscreen();
+    filled = false;
+  } else if (body.mozExitFullScreen) {
+    body.mozExitFullScreen();
+    filled = false;
+  } else if (body.webkitExitFullscreen) {
+    body.webkitExitFullscreen();
+    filled = false;
+  } else if (body.msExitFullscreen) {
+    body.msExitFullscreen();
+    filled = false;
+  }
+  } */
+}
+
+function clearAll() {
+  window.clearInterval(myInterval);
+}
+
+function automatic() {
+  if (autoOn == false) {
+    autoOn = true;
+  myInterval = window.setInterval(nextDigit, 100);
+} else {
+  autoOn = false;
+  window.clearInterval(myInterval);
+  clearAll();
+}
+}
